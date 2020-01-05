00c2f070732754702cf6ce814f25e37c6a2fd731
Pawel Wamej <pawelw@qualtrics.com>
2020-01-05 17:52:35 +0100

    add registration by link

--- memeboard.py
+++ memeboard.py
@@ -1,5 +1,7 @@
+from uuid import uuid4
+
 from authlib.integrations.flask_client import OAuth
-from flask import Flask, url_for, redirect, abort, request, g, session
+from flask import Flask, url_for, redirect, abort, request, g, session, current_app
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
@@ -72,6 +74,37 @@ def authorize():
     return redirect(next_uri)
 
 
+@app.route('/register')
+def register():
+    registration_id = request.args['id']
+    registration_record = RegistrationLink.query.filter_by(id=registration_id).first()
+    if not registration_record:
+        abort(404)
+    session['registration_id'] = registration_id
+    redirect_uri = url_for('authorize_register', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize-register')
+def authorize_register():
+    token = oauth.google.authorize_access_token()
+    user_info = oauth.google.parse_id_token(token)
+    user_id = user_info['sub']
+    if load_user(user_id):
+        abort(409)
+    user = User(
+        id=user_id,
+        email=user_info['email'],
+        roles=[Role(id=role.USER)]
+    )
+    registration_id = session['registration_id']
+    db.session.add(user)
+    RegistrationLink.query.filter_by(id=registration_id).delete()
+    db.session.commit()
+    login_user(user)
+    return redirect('/')
+
+
 roles = db.Table(
     'roles',
     db.Column('user_id', db.String(64), db.ForeignKey('user.id'), primary_key=True),
@@ -103,6 +136,11 @@ class Role(db.Model):
         return self.id
 
 
+class RegistrationLink(db.Model):
+    id = db.Column(db.String(32), primary_key=True)
+    link = db.Column(db.String(128), nullable=False)
+
+
 class SecuredAdminIndexView(AdminIndexView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
@@ -127,7 +165,24 @@ class SecuredModelView(ModelView):
             abort(403)
 
 
+class RegistrationLinkView(SecuredModelView):
+    form_columns = ['id', 'link']
+
+    def create_form(self, obj=None):
+        form = super().create_form(obj)
+        if not obj:
+            uuid = uuid4().hex
+            form.id.data = uuid
+            with current_app.app_context():
+                form.link.data = url_for('register', id=uuid, _external=True)
+        return form
+
+    def __init__(self, db_session):
+        super().__init__(RegistrationLink, db_session)
+
+
 # Configure admin view
 admin = Admin(app, name='Memeboard Admin', template_mode='bootstrap3', index_view=SecuredAdminIndexView())
 admin.add_view(SecuredModelView(Role, db.session))
 admin.add_view(SecuredModelView(User, db.session))
+admin.add_view(RegistrationLinkView(db.session))
--- test_memeboard.py
+++ test_memeboard.py
@@ -6,6 +6,8 @@ from pytest import fixture
 from role import USER, ADMIN
 
 TEST_SUB = 'user-id'
+TEST_ID = 'registration_id'
+TEST_MAIL = 'test@mail.com'
 
 
 def test_index_redirects_unauthorized_user_to_login(client):
@@ -16,13 +18,29 @@ def test_index_redirects_unauthorized_user_to_login(client):
 
 def test_unregistered_user_can_not_login(client):
     login_res = client.get('/login', follow_redirects=True)
-    assert login_res.status_code == 401
+    assert login_res.status_code == 403
     index_res = client.get('/')
     assert index_res.status_code != 200
 
 
+def test_user_cannot_register_without_record(client):
+    res = client.get(f'/register?id={TEST_ID}', follow_redirects=True)
+    assert res.status_code == 404
+
+
+def test_user_registration(client):
+    from memeboard import RegistrationLink, User, Role
+
+    add_registration_link(client, TEST_ID)
+    res = client.get(f'/register?id={TEST_ID}', follow_redirects=True)
+    assert not RegistrationLink.query.filter_by(id=TEST_ID).first()
+    assert User.query.filter_by(id=TEST_SUB).first() == User(id=TEST_SUB, email=TEST_MAIL, roles=[Role(id=USER)])
+    assert res.status_code == 200
+    assert request.path == '/'
+
+
 def test_user_redirected_to_index_after_login(client):
-    add_user(client.db, TEST_SUB, 'test@mail.com')
+    add_user(client.db, TEST_SUB, TEST_MAIL)
     res = client.get('/login', follow_redirects=True)
     assert res.status_code == 200
     assert request.path == '/'
@@ -40,6 +58,7 @@ def client(mock_oauth, monkeypatch):
     monkeypatch.setattr(memeboard, 'oauth', mock_oauth)
     with app.test_client() as client:
         client.db = db
+        client.app = app
         yield client
     db.drop_all()
 
@@ -48,15 +67,25 @@ def client(mock_oauth, monkeypatch):
 def mock_oauth():
     oauth = Mock()
     oauth.google.authorize_redirect = Mock(side_effect=redirect)
-    oauth.google.parse_id_token = Mock(return_value={'sub': TEST_SUB})
+    oauth.google.parse_id_token = Mock(return_value=dict(sub=TEST_SUB, email=TEST_MAIL))
     return oauth
 
 
 def add_user(db, user_id, email, is_admin=False):
-    from memeboard import UserRole, User
+    from memeboard import Role, User
 
-    roles = [USER, ADMIN] if is_admin else [USER]
-    user_roles = [UserRole(role=r) for r in roles]
-    user = User(id=user_id, email=email, roles=user_roles)
+    role_slugs = [USER, ADMIN] if is_admin else [USER]
+    roles = [Role(id=r) for r in role_slugs]
+    user = User(id=user_id, email=email, roles=roles)
     db.session.add(user)
     db.session.commit()
+
+
+def add_registration_link(client, registration_id):
+    from memeboard import RegistrationLink
+
+    with client.app.test_request_context():
+        link = url_for('register', id=registration_id, _external=True)
+    link_record = RegistrationLink(id=registration_id, link=link)
+    client.db.session.add(link_record)
+    client.db.session.commit()
